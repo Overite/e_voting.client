0bf7ebd7ba5b8ac72c0b31ed84addc423b7ebf0f
Victor <vicky.robilson5528@gmail.com>
2024-02-19 10:09:50 +0100

    added fingerprint implementation

--- app/recover_my_wallet/index.tsx
+++ app/recover_my_wallet/index.tsx
@@ -1,8 +1,8 @@
 
-import React, { useState,useRef } from 'react';
+import React, { useState,useRef, useEffect} from 'react';
 import { Text,View } from '@/components/Themed'
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import { utils_styles } from "@/constants/utils_styles";
 import { e_voting_green } from "@/constants/Colors";
 import use_ubuntu_font from "@/hooks/fonts/ubuntu_medium_font";
@@ -10,33 +10,109 @@ import { useAppDispatch } from "@/hooks/state/use_base_hooks";
 import { TextInput, Alert,Image,TouchableOpacity } from 'react-native';
 import {images} from '@/constants/images'
 import { RNCamera } from 'react-native-camera';
-
+import FingerprintScanner from 'react-native-fingerprint-scanner';
+import BiometricRecords from 'react-native-fingerprint-scanner'
+import * as LocalAuthentication from 'expo-local-authentication';
+import use_lora_font from '@/hooks/fonts/lora_font';
+import { Camera, CameraType } from 'expo-camera';
+import  {FaceDetector, FaceDetectionResult}  from 'expo-face-detector';
 
 function recover_my_wallet() {
   const [text, setText] = useState('')
-  const [isCameraReady, setCameraReady] = useState(false);
-  const cameraRef =  useRef<RNCamera | null>(null);
+  const [type, setType] = useState(CameraType.back);
+  const cameraRef = useRef<Camera>(null);
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [verifyButtonOpacity, setVerifyButtonOpacity] = useState<number>(0);
+  const [scanningMode, setScanningMode] = useState<string | null>(null)
 
 
-  const handleTakePicture = async () => {
-    if (isCameraReady && cameraRef.current) {
-      try {
-        const { uri } = await cameraRef.current.takePictureAsync();
-        // Use the captured image uri for face recognition or other processing
-        console.log('Picture taken:', uri);
-      } catch (error) {
-        console.error('Error taking picture:', error);
-      }
-    }
-  };
+  const { } = use_lora_font();
+  const { } = use_ubuntu_font();
+
   const handleInputChange = (inputText:any) => {
     setText(inputText);
   };
+
 
   const handleButtonPress = () => {
     // Do something with the input text, e.g., show it in an alert
     Alert.alert('Entered Text', text);
   };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleFacesDetected = (result: FaceDetectionResult) => {
+    if (result.faces.length > 0) {
+      console.log('Face detected!');
+      setVerifyButtonOpacity(1); // Set opacity to 1 when face is detected
+      // Add your logic for face detection success here
+    }
+  };
+
+  
+  const handleFaceScanning = async () => {
+    if (cameraRef.current) {
+      setScanningMode('face')
+      const options = {
+        mode: FaceDetector?.Constants?.Mode.fast,
+        detectLandmarks: FaceDetector?.Constants?.Landmarks.none,
+        runClassifications: FaceDetector?.Constants?.Classifications.none,
+        minDetectionInterval: 1000,
+        tracking: true,
+      };
+
+      // Start face detection
+      await cameraRef.current.resumePreview();
+      cameraRef.current.startFaceDetectionAsync(options);
+    }
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+  
+  const handleVerifyButtonPress = () => {
+    // Add your logic for verifying the voter
+    console.log('Verify button pressed!');
+  };
+
+  const handleBiometricScan = async () => {
+    try {
+      const hasBiometric = await LocalAuthentication.hasHardwareAsync();
+      const isEnrolled = await LocalAuthentication.isEnrolledAsync();
+
+      if (hasBiometric && isEnrolled) {
+        setScanningMode('fingerprint')
+        const result = await LocalAuthentication.authenticateAsync({
+          promptMessage: 'Authenticate to continue',
+        });
+
+        if (result.success) {
+          console.log('Biometric authentication successful');
+        } else {
+          console.log('Biometric authentication failed');
+        }
+      } else {
+        console.log('Biometric not available or not enrolled');
+      }
+    } catch (error) {
+      console.error('Error during biometric authentication:', error);
+    }
+  };
+
+  const handleFingerprintScanning = () => {
+    setScanningMode('fingerprint'); // Set scanning mode to 'fingerprint'
+    // Add your logic for fingerprint scanning here
+  };
+
+
+ 
+
 
   return (
     <SafeAreaView style={styles.safe_area_styles}>
@@ -52,13 +128,41 @@ function recover_my_wallet() {
       <View style={styles.gapBottom}>
        <Text style={styles.facialTitle}>Facial</Text>
        <View style={styles.facialInput}>
-       {/* <RNCamera
-        ref={(ref) => (cameraRef.current = ref)}
-        style={{ flex: 1 }}
-        type={RNCamera.Constants.Type.front}
-        onCameraReady={() => setCameraReady(true)}
-      /> */}
-      <TouchableOpacity onPress={handleTakePicture} style={{ padding: 16 }}>
+       <View style={{ flex: 1 }}>
+      {hasPermission === null ? (
+        <View />
+      ) : hasPermission === false ? (
+        <Text>No access to camera</Text>
+      ) : (
+        <View>
+          {scanningMode === 'face' && (
+        <Camera
+          ref={cameraRef}
+          style={{ flex: 1 }}
+          type={type}
+          onFacesDetected={handleFacesDetected}
+          faceDetectorSettings={{
+            mode: FaceDetector?.Constants?.Mode.fast,
+            detectLandmarks: FaceDetector?.Constants?.Landmarks.none,
+            runClassifications: FaceDetector?.Constants?.Classifications.none,
+            minDetectionInterval: 1000,
+            tracking: true,
+          }}
+        />
+        )}
+        
+          <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
+            <TouchableOpacity style={{ flex: 0.1, alignSelf: 'flex-end', alignItems: 'center' }} onPress={handleFaceScanning} >
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>Scan Face</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </View>
+
+        
+       
+      <TouchableOpacity  onPress={toggleCameraType} style={{ padding: 16 }}>
        <Image 
        source={images.user}
       />
@@ -68,13 +172,20 @@ function recover_my_wallet() {
       </View>
        <Text style={styles.fingerprintTitle}>Fingerprint</Text>
        <View style={styles.fingerprint_Input}>
+       <TouchableOpacity onPress={handleBiometricScan}>
        <Image 
        source={images.finger_print} 
        height={72}
        width={72}
        />
        <Text style={styles.clickToProceed}>Click to proceed</Text>
+      </TouchableOpacity>
        </View>
+       <TouchableOpacity style={styles.wallet_btn}
+       onPress={handleVerifyButtonPress}>
+        <Text style={styles.title_btn}>Verify</Text>
+       </TouchableOpacity>
+      
       </View>
     </SafeAreaView>
   )
@@ -172,9 +283,27 @@ const styles = StyleSheet.create({
       alignContent:'center',
       height:'100%',
       width:'100%',
-
+    },
+    wallet_btn:{
+      width:366,
+      marginTop:40,
+      height:42,
+      borderRadius:4,
+      paddingTop:8,
+      paddingBottom:8,
+      paddingLeft:24,
+      paddingRight:24,
+      gap:10,
+      backgroundColor:'#276221'
+    },
+    title_btn:{
+      color:'#FFFEFC',
+      fontWeight:'700',
+      fontSize:16,
+      lineHeight:20.48,
+      textAlign:'center',
+      fontFamily:'lora-bold'
     }
-   
 })
 
 export default recover_my_wallet
